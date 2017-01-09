be4259e3b9fb4f07522f656f3315d5bbfbb80e96
Bhaskar Dua <bhaskar_dua@yahoo.com>
2017-01-09 21:13:39 +0800

    feat: Add a single marker to map

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,25 @@
-// eslint-disable-next-line no-unused-vars
 var map;
 // eslint-disable-next-line no-unused-vars
 function initMap() {
     // Constructor creates a new map - only center and zoom are required.
-    /* global google */ 
+    /* global google */
+    var singapore = {
+        lat: 1.280977,
+        lng: 103.851357
+    };
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {
-            lat: 1.280977,
-            lng: 103.851357 
-        },
-        zoom: 13
+        center: singapore,
+        zoom: 12
+    });
+// eslint-disable-next-line no-unused-vars
+    var marker = new google.maps.Marker({
+        position: singapore,
+        map: map
+    });
+    var infowindow = new google.maps.InfoWindow({
+        content:  'Hello, World'
+    });
+    marker.addListener('click', function() {
+        infowindow.open(map, marker);
     });
 }
