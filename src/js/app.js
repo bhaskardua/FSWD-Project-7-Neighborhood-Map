0b72e5065ffbffd3e06b329d9f414afeb9a65bf5
Bhaskar Dua <bhaskar_dua@yahoo.com>
2017-01-10 07:10:10 +0800

    feat: Generate dynamic list view for markers on map using ko.ObservableArray feat: Add styling for the list view

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,11 @@
 var markersArray = [];
 var map;
 var infowindow;
+var service;
+
 // eslint-disable-next-line no-unused-vars
 function initMap() {
-    /* global google */
+    /* global google ko */
     var singapore = {
         lat: 1.280977,
         lng: 103.851357
@@ -18,51 +20,72 @@ function initMap() {
             position: google.maps.ControlPosition.LEFT_BOTTOM
         }
     });
-    
+
+    // eslint-disable-next-line no-undef
+    ko.applyBindings(new ListingsViewModel());
+}
+
+function ListingsViewModel() {
+    var self = this;
+
+    self.Listings = ko.observableArray([]);
+
     // Places
+    //
     infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
+
+    service = new google.maps.places.PlacesService(map);
+
     google.maps.event.addListener(map, 'idle', function() {
+
         clearOverlays();
+
         service.nearbySearch({
             bounds: map.getBounds(),
             type: ['restaurant']
         }, callback);
+
         service.nearbySearch({
             bounds: map.getBounds(),
             type: ['bar']
         }, callback);
-    });
-}
 
-function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
+    });
+
+    function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+            }
         }
     }
-}
 
-function createMarker(place) {
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location,
-    });
+    function createMarker(place) {
+        var marker = new google.maps.Marker({
+            map: map,
+            position: place.geometry.location,
+        });
 
-    markersArray.push(marker);
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-        marker.setAnimation(google.maps.Animation.BOUNCE);
-        window.setTimeout(function() {
-            marker.setAnimation(null);
-        }, 1400);
-    });
-}
+        markersArray.push(marker);
 
-function clearOverlays() {
-    for (var i = 0; i < markersArray.length; i++ ) {
-        markersArray[i].setMap(null);
+        self.Listings.push({ name: place.name });
+
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.setContent(place.name);
+            infowindow.open(map, this);
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            window.setTimeout(function() {
+                marker.setAnimation(null);
+            }, 1400);
+        });
     }
-    markersArray.length = 0;
+
+// eslint-disable-next-line no-unused-vars
+    function clearOverlays() {
+        for (var i = 0; i < markersArray.length; i++ ) {
+            markersArray[i].setMap(null);
+        }
+        markersArray.length = 0;
+        self.Listings([]);
+    }
 }
