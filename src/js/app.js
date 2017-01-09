527f395d7916757ae03a65b8e61b5ba98869885e
Bhaskar Dua <bhaskar_dua@yahoo.com>
2017-01-10 04:04:11 +0800

    feat: Add places services for fetching marker data fix: Show markers in viewport only

--- src/js/app.js
+++ src/js/app.js
@@ -1,39 +1,68 @@
+var markersArray = [];
 var map;
+var infowindow;
 // eslint-disable-next-line no-unused-vars
 function initMap() {
-    // Constructor creates a new map - only center and zoom are required.
     /* global google */
     var singapore = {
         lat: 1.280977,
         lng: 103.851357
     };
+
+    // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
         center: singapore,
-        zoom: 12,
+        zoom: 16,
         mapTypeControl: true,
         mapTypeControlOptions: {
             position: google.maps.ControlPosition.LEFT_BOTTOM
         }
     });
-// eslint-disable-next-line no-unused-vars
+    
+    // Places
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    google.maps.event.addListener(map, 'idle', function() {
+        clearOverlays();
+        service.nearbySearch({
+            bounds: map.getBounds(),
+            type: ['restaurant']
+        }, callback);
+        service.nearbySearch({
+            bounds: map.getBounds(),
+            type: ['bar']
+        }, callback);
+    });
+}
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
     var marker = new google.maps.Marker({
         map: map,
-        position: singapore,
-        animation: google.maps.Animation.DROP
+        position: place.geometry.location,
     });
-    var infowindow = new google.maps.InfoWindow({
-        content:  'Hello, World'
-    });
-    marker.addListener('click', function() {
-        infowindow.open(map, marker);
-        // if (marker.getAnimation() !== null) {
-        //     marker.setAnimation(null);
-        // } else {
-        //     marker.setAnimation(google.maps.Animation.BOUNCE);
-        // }
+
+    markersArray.push(marker);
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
         marker.setAnimation(google.maps.Animation.BOUNCE);
         window.setTimeout(function() {
             marker.setAnimation(null);
         }, 1400);
     });
+}
+
+function clearOverlays() {
+    for (var i = 0; i < markersArray.length; i++ ) {
+        markersArray[i].setMap(null);
+    }
+    markersArray.length = 0;
 }
