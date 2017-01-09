8074264e55a9e8fd5ceb886d83b537edbf46668f
Bhaskar Dua <bhaskar_dua@yahoo.com>
2017-01-10 02:09:06 +0800

    feat: Add animation to marker

--- src/js/app.js
+++ src/js/app.js
@@ -9,17 +9,31 @@ function initMap() {
     };
     map = new google.maps.Map(document.getElementById('map'), {
         center: singapore,
-        zoom: 12
+        zoom: 12,
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            position: google.maps.ControlPosition.LEFT_BOTTOM
+        }
     });
 // eslint-disable-next-line no-unused-vars
     var marker = new google.maps.Marker({
+        map: map,
         position: singapore,
-        map: map
+        animation: google.maps.Animation.DROP
     });
     var infowindow = new google.maps.InfoWindow({
         content:  'Hello, World'
     });
     marker.addListener('click', function() {
         infowindow.open(map, marker);
+        // if (marker.getAnimation() !== null) {
+        //     marker.setAnimation(null);
+        // } else {
+        //     marker.setAnimation(google.maps.Animation.BOUNCE);
+        // }
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        window.setTimeout(function() {
+            marker.setAnimation(null);
+        }, 1400);
     });
 }
