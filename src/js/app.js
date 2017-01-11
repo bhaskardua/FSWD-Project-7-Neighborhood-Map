9f8874ed23c41148a88fa97f26be11aca99bd727
Bhaskar Dua <bhaskar_dua@yahoo.com>
2017-01-12 04:02:10 +0800

    feat: Add 3rd party (foursquare) API feat: Use jQuery for AJAX calls to APIs feat: Add error-proofing using the jQuery .fail() methods feat: Filter list view

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,23 @@
-var markersArray = [];
 var map;
 var infowindow;
 var service;
+var FS_API_URLRoot = 'https://api.foursquare.com/v2/venues/search?';
+/* global google ko */
 
-// eslint-disable-next-line no-unused-vars
-function initMap() {
-    /* global google ko */
+// ViewModel
+function ListingsViewModel() {
+
+    var self = this;
+
+    self.markersArray = ko.observableArray([]);
+    self.markerFilterText = ko.observable();
+
     var singapore = {
         lat: 1.280977,
         lng: 103.851357
     };
 
-    // Constructor creates a new map - only center and zoom are required.
+    // Constructor creates a new map
     map = new google.maps.Map(document.getElementById('map'), {
         center: singapore,
         zoom: 16,
@@ -21,17 +27,8 @@ function initMap() {
         }
     });
 
-    // eslint-disable-next-line no-undef
-    ko.applyBindings(new ListingsViewModel());
-}
+    // Places search
 
-function ListingsViewModel() {
-    var self = this;
-
-    self.Listings = ko.observableArray([]);
-
-    // Places
-    //
     infowindow = new google.maps.InfoWindow();
 
     service = new google.maps.places.PlacesService(map);
@@ -64,28 +61,106 @@ function ListingsViewModel() {
         var marker = new google.maps.Marker({
             map: map,
             position: place.geometry.location,
+            placeName: place.name,
+            filtered: ko.observable(true),
         });
 
-        markersArray.push(marker);
+        self.markersArray.push(marker);
 
-        self.Listings.push({ name: place.name });
-
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.setContent(place.name);
-            infowindow.open(map, this);
-            marker.setAnimation(google.maps.Animation.BOUNCE);
-            window.setTimeout(function() {
-                marker.setAnimation(null);
-            }, 1400);
-        });
+        google.maps.event.addListener(marker, 'click', self.markerClick);
     }
 
-// eslint-disable-next-line no-unused-vars
-    function clearOverlays() {
-        for (var i = 0; i < markersArray.length; i++ ) {
-            markersArray[i].setMap(null);
+    // eslint-disable-next-line no-unused-vars
+    self.markerClickKO = function(data) {
+        self.markerClick.call(data);
+    };
+
+    // Click event responder
+    self.markerClick = function () {
+        var marker = this;
+        infowindow.setContent(marker.placeName);
+        infowindow.open(map, marker);
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            marker.setAnimation(null);
+        }, 1400);
+
+        //Foursqaure API URL preparation
+        var params = {
+            ll            : marker.position.lat() + ',' + marker.position.lng(),
+            intent        : 'match',
+            query         : marker.placeName,
+            client_id     : '42K0YU5ZOS1HHB4OA0OYAMURDGN0FPH5DEQOT0XLJHWXPJ0O',
+            client_secret : '2TBSDEB0WLTOEJF1C3KVLAPNAEGYYL1JF2XGCN3KGCP413EL',
+            v             : '20170111'
+        };
+
+        // eslint-disable-next-line no-undef
+        var FS_API_URL = FS_API_URLRoot + $.param(params);
+
+        // AJAX call to Foursqaure API
+        // eslint-disable-next-line no-undef
+        $.ajax(FS_API_URL).done(function(response) {
+
+            var name           = response.response.venues[0].name;
+            var address        = response.response.venues[0].location.address;
+            var crossStreet    = response.response.venues[0].location.crossStreet;
+            var postalCode     = response.response.venues[0].location.postalCode;
+            var formattedPhone = response.response.venues[0].contact.formattedPhone;
+            var url            = response.response.venues[0].url;
+
+            var content = '<strong style="font-size:14px">' + name + '</strong>' + '<br><br>' +
+                '<em>' + (address ? address : '') + '</em>' + '<br>' +
+                '<em>' + (crossStreet ? crossStreet : '') + '</em>' + '<br>' +
+                '<em>' + (postalCode ? postalCode : '') + '</em>' + '<br><br>' +
+                '<u>' + (formattedPhone ? formattedPhone : '') + '</u>' + '<br><br>' +
+                '<a href="' + url + '">' + (url ? url : '') + '</a><br><br>' +
+                '<em style="font-size:9px">Data provided by <a href="https://www.foursquare.com" target="_blank">Foursquare</a></em>';
+
+            infowindow.setContent(content);
+
+        }).fail(function() {
+            var content = '<strong>' + marker.name + '</strong>' + '<br><br>' +
+                '<em>Foursquare data not accessible</em>';
+
+            infowindow.setContent(content);
+
+        });
+    };
+
+    // Filter function
+    self.markersFilter = function() {
+        if (self.markerFilterText()) {
+            for (var i = 0; i < self.markersArray().length; i++ ) {
+                if (!~self.markersArray()[i].placeName.toLowerCase().indexOf(self.markerFilterText().toLowerCase())) {
+                    self.markersArray()[i].setMap(null);
+                    self.markersArray()[i].filtered(false);
+                }
+            }
+        } else {
+            clearFilter();
         }
-        markersArray.length = 0;
-        self.Listings([]);
+    };
+
+    // User Helper functions
+    
+    function clearFilter() {
+        for (var i = 0; i < self.markersArray().length; i++ ) {
+            self.markersArray()[i].setMap(map);
+            self.markersArray()[i].filtered(true);
+        }
     }
+
+    function clearOverlays() {
+        for (var i = 0; i < self.markersArray().length; i++ ) {
+            self.markersArray()[i].setMap(null);
+        }
+        self.markersArray([]);
+    }
+}
+
+// Callback function for the async Google Maps API call
+// eslint-disable-next-line
+function initView() {
+    ko.applyBindings(new ListingsViewModel());
 }
